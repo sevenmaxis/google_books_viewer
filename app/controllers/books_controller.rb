9d67b0477b8a43aaba96c0a71c932e4f552fcc2f
Sergey Gopkalo <Sergey.Gopkalo@gmail.com>
2013-05-20 00:41:10 +0300

    fix the css style

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,14 @@ class BooksController < ApplicationController
   respond_to :html
 
   def index
-    @query, page = params[:query], params[:page].to_i
-    
-    @books, total = Book.search(@query, page) do
+    @query, page = params[:query], params[:page]
+    page = 1 unless page
+
+    @books, @total = Book.search(@query, page) do
       GoogleBooks.search(@query, { page: page })
     end
 
-    @books = Kaminari.paginate_array(@books, total_count: total).page(page).per(5)
+    @books = Kaminari.paginate_array(@books, total_count: @total).page(page).per(5)
 
     respond_with @books
   end
