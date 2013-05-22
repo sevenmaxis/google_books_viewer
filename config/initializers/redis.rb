8db0f4a83d15471b8298bf005024e6fdb8667d59
Sergey Gopkalo <Sergey.Gopkalo@gmail.com>
2013-05-23 01:44:31 +0300

    fix configuration for redis and assets precompile for production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,11 @@
-$redis = Redis.new( host:     Settings.redis.host, 
-                    port:     Settings.redis.port,
-                    passowrd: Settings.redis.passowrd )
+if Rails.env == "production"
+  uri = URI.parse(ENV['REDISTOGO_URL'])
+  $redis = Redis.new( host:     uri.host,
+                      port:     uri.port,
+                      passowrd: uri.passowrd)
+else
+  $redis = Redis.new( host:     Settings.redis.host, 
+                      port:     Settings.redis.port,
+                      passowrd: Settings.redis.passowrd )
+end
 
